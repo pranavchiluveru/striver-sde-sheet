1b0eaa293c431277646a0cd791955f732b68c477
pranavchiluveru <51951094+pranavchiluveru@users.noreply.github.com>
2023-06-05 12:12:30 +0530

    Update set matrix zeros.cpp

--- set matrix zeros.cpp
+++ set matrix zeros.cpp
@@ -10,16 +10,16 @@ void setZeros(vector<vector<int>> &matrix)
 	{
 		for(int j=0;j<m;j++)
 		{
-       if (matrix[i][j] == 0) 
+                         if (matrix[i][j] == 0) 
 			 {
 				matrix[i][0]=0; 
 				if(j!=0)
-				matrix[0][j]=0;
+				    matrix[0][j]=0;
 				else
-				col1=0; 
-        }
-     }
-	 }
+				    col1=0; 
+                         }
+                }
+	}
 	for(int i=1;i<n;i++)
 	{
 		for(int j=1;j<m;j++)
